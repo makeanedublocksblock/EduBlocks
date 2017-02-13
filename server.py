bdff9e0e7af3ba50ef69b26fa69b066d781a8b1a
Chris Dell <cjdell@gmail.com>
2017-02-13 20:31:11 +0000

    Enable XHR to work remotely and locally

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import bottle
 from bottle import *
 import os
 
@@ -5,14 +6,22 @@ app = Bottle()
 
 blockly_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ui')
 
-@app.hook('after_request')
-def enable_cors():
-    print("after_request hook")
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+# the decorator
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
 
 @post('/runcode')
+@enable_cors
 def do_login():
     codeToRun = request.forms.get('code')
     print(codeToRun)
