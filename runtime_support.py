fc0a181a2ca5943ef0d1cbec337e39245699f51d
AllAboutCode <allaboutcodeorg@gmail.com>
2017-02-14 13:40:30 +0000

    Added pumpkin feature back into runtime_support.py

--- runtime_support.py
+++ runtime_support.py
@@ -5,3 +5,26 @@ def distance_to_player(x, y, z):
   yd = y - pp.y
   zd = z - pp.z
   return math.sqrt((xd * xd) + (yd * yd) + (zd * zd))
+
+def buildPumpkin(x, y, z):
+  mc.setBlocks(x-2, y-2, z-2, x+2, y+2, z+2, 35, 1)
+  mc.setBlocks(x-1, y-1, z-1, x+1, y+1, z+1, 0, 1)
+  mc.setBlock(x-1, y+1, z-2, 0)
+  mc.setBlock(x+1, y+1, z-2, 0)
+
+  mc.setBlocks(x+1, y-1, z-2, x-1, y-1, z-2, 0, 0)
+  mc.setBlock(x-1, y+1, z+2, 0)
+  mc.setBlock(x+1, y+1, z+2, 0)
+
+  mc.setBlocks(x+1, y-1, z+2, x-1, y-1, z+2, 0, 0)
+  mc.setBlock(x-2, y+1, z-1, 0)
+  mc.setBlock(x-2, y+1, z+1, 0)
+
+  mc.setBlocks(x-2, y-1, z+1, x-2, y-1, z-1, 0, 0)
+  mc.setBlock(x+2, y+1, z-1, 0)
+  mc.setBlock(x+2, y+1, z+1, 0)
+
+  mc.setBlocks(x+2, y-1, z+1, x+2, y-1, z-1, 0, 0)
+  mc.setBlock(x, y+3, z, 35, 5)
+  
+
