87b803954d13c61f94862d0233db6306f2f24a4f
AllAboutCode <allaboutcodeorg@gmail.com>
2017-02-17 10:37:54 +0000

    Added beta.sh

---
+++ beta.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Edu_blocks install script
+#Les Pounder September 17 2016
+#Modified by Joshua Lowe February 13 2017
+#V1.2 SLATE UI (BETA RELEASE CHANNEL)
+# If you cannot understand this, read Bash_Shell_Scripting#if_statements again.
+if (whiptail --title "EduBlocks Installer" --yesno "Welcome to the EduBlocks installer. THIS INSTALLS BETA SOFTWARE, IT MAY HAVE BUGS AND IS NOT 100% TESTED! NOT FOR THE FAINT HEARTED. Do you want to continue? PASSWORD REQUIRED" 8 78) then
+    PASSWORD=$(whiptail --passwordbox "jacu5" 8 78 --title "Please enter your EduBlocks Beta Tester password!" 3>&1 1>&2 2>&3)
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        echo "User selected Ok and entered " $PASSWORD
+        echo "(Exit status was $exitstatus)"
+        echo "Downloading the package"
+        git clone https://github.com/AllAboutCode/edublocks
+        echo "Copying the desktop shortcut to your desktop"
+        sudo cp edublocks.desktop ./Desktop
+        echo "Copying the icon."
+        sudo cp /ui/logo.png /usr/share/icons/hicolor/scalable/apps/logo.png
+        echo "Making the program visable in the menu."
+        sudo cp edublocks.desktop /usr/share/applications
+        echo "Downloading the edupy library"
+        sudo pip3 install edupy
+    else
+        echo "User selected Cancel."
+    fi
+
+else
+    echo "Sorry to hear you won't be installing EduPython today! If you want to install from the main release channel type: wget http://edupython.co.uk/install.sh && sudo bash install.sh"
+fi
+
+whiptail --title "EduBlocks Installer" --msgbox "Congratulations! EduBlocks has successfully installed. To get coding double click on the EduBlocks desktop icon." 8 78
+
+
+
