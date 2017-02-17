f182685cc17a8487f7b5f73374c139bbf29f974e
Chris Dell <cjdell@gmail.com>
2017-02-17 13:45:28 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 echo "Downloading the package"
-git clone https://github.com/AllAboutCode/edublocks
 
-cd edublocks
+REPOSRC=https://github.com/AllAboutCode/edublocks
+LOCALREPO=~/edublocks
+
+# We do it this way so that we can abstract if from just git later on
+LOCALREPO_VC_DIR=$LOCALREPO/.git
+
+if [ ! -d $LOCALREPO_VC_DIR ]
+then
+    git clone $REPOSRC $LOCALREPO
+else
+    cd $LOCALREPO
+    git pull $REPOSRC
+fi
+
+cd $LOCALREPO
 bash ./install.sh
