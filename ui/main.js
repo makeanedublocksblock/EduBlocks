fc83811fc292f0fc313fdab31568d6513311aa73
Chris Dell <cjdell@gmail.com>
2017-02-17 13:06:54 +0000

    Electron window size

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,9 @@ let mainWindow
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({ width: 1024, height: 768 })
+
+  mainWindow.maximize();
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -37,7 +39,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow) 
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
