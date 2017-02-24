f8370e2fcbd043a309c2d614941704823b90c62a
Chris Dell <cjdell@gmail.com>
2017-02-24 09:48:16 +0000

    Terminal bug fixes

--- server/index.ts
+++ server/index.ts
@@ -10,6 +10,7 @@ import { Readable } from 'stream';
 const ui = path.join(__dirname, '..', 'ui');
 const runtimeSupportPath = path.join(__dirname, '..', 'runtime_support.py');
 const scriptPath = path.join(__dirname, '..', 'tmp', 'output.py');
+const packagePath = path.join(__dirname, 'package.json');
 
 const app = express();
 
@@ -53,6 +54,10 @@ app.post('/runcode', (req, res) => {
 
   proc.stderr.on('data', (data) => {
     console.log(`stderr: ${data}`);
+
+    if (lineReporter) {
+      lineReporter('ERROR: ' + data);
+    }
   });
 
   proc.on('close', (code) => {
@@ -69,7 +74,13 @@ app.post('/runcode', (req, res) => {
       if (inp.length === 1 && inp[0] === String.fromCharCode(3)) {
         proc.kill('SIGTERM');
       } else {
-        proc.stdin.write(inp);
+        // Echo the send command back to the client
+        if (lineReporter) {
+          lineReporter(inp);
+        }
+
+        // Need to add new line character on end
+        proc.stdin.write(inp + '\n');
       }
     } catch (e) { }
   };
@@ -81,6 +92,10 @@ app.ws('/terminal', (ws, req: express.Request) => {
       ws.send(msg);
     } catch (e) { }
   };
+
+  const version = JSON.parse(fs.readFileSync(packagePath, 'utf8')).version;
+
+  ws.send(`EduBlocks server version ${version} online and ready`);
 
   ws.on('message', (msg) => {
     console.log(`INP: ${msg}`);
